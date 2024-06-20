393e30fa5d6d5e263a0a276303c6856c09156b9e
upaaman <upadhyayaman9685@gmail.com>
2024-06-20 13:27:05 +0530

    changes that are not required

---
+++ pages/login.js
@@ -0,0 +1,25 @@
+import { useForm } from 'react-hook-form';
+
+function login() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+  
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>Email</label>
+      <input type="email" {...register("email", { required: true, pattern: /^\S+@\S+$/i })} />
+      {errors.email && <p>Email is required and must be valid</p>}
+      
+      <label>Password</label>
+      <input type="password" {...register("password", { required: true })} />
+      {errors.password && <p>Password is required</p>}
+      
+      <button type="submit">Submit</button>
+    </form>
+  );
+}
+
+export default login;
--- pages/wishList.js
+++ pages/wishList.js
@@ -7,7 +7,7 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 const wishList = () => {
-    const dispatch = useDispatch();
+    const dispatch=useDispatch();
     const { wishListItems } = useSelector((state => state.wishList))
     // console.log("WishList Items", wishListItems);
     return (

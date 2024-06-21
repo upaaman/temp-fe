3851e7719b52b048fe4dd4a43e37e3d611dcf095
upaaman <upadhyayaman9685@gmail.com>
2024-06-21 16:28:07 +0530

    changing the static page generation timeout

--- next.config.js
+++ next.config.js
@@ -8,5 +8,10 @@ images: {
     domains: ["res.cloudinary.com"],
 },
 }
+module.exports = {
+  // time in seconds of no pages generating during static
+  // generation before timing out
+  staticPageGenerationTimeout: 1000,
+}
 
 module.exports = nextConfig

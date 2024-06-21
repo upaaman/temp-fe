c127797aa1a4b63c7ac35d6ff08f9209fcecbe49
upaaman <upadhyayaman9685@gmail.com>
2024-06-21 16:40:57 +0530

    this is second git repo for the previous project

--- next.config.js
+++ next.config.js
@@ -8,10 +8,6 @@ images: {
     domains: ["res.cloudinary.com"],
 },
 }
-module.exports = {
-  // time in seconds of no pages generating during static
-  // generation before timing out
-  staticPageGenerationTimeout: 1000,
-}
+
 
 module.exports = nextConfig
